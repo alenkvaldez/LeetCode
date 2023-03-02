50e53cf56237086260de481f094c2e33ad481d20
Alen Valdez <116237490+alenkvaldez@users.noreply.github.com>
2023-03-02 14:55:04 -0800

    Time: 68 ms (84.66%), Space: 42.8 MB (36.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,17 +4,30 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const hash = {};
-
-    for(let i = 0; i < nums.length; i++){
-        let val = nums[i];
-        hash[val] = i;
-    }
-
-    for(let i = 0; i < nums.length; i++){
-        let potentialKey = target - nums[i];
-
-        if(hash[potentialKey] && hash[potentialKey] !== i){return [i, hash[potentialKey]];
+    let numbers = {};
+    for (let i = 0; i < nums.length; i++){
+        let currentNum = nums[i];
+        let neededNum = target - currentNum;
+        if (neededNum in numbers){
+            return [numbers[neededNum], i]
+            } else {
+                numbers[currentNum] = i;
+            }
         }
-    }
+    return [];
 };
+
+
+/*
+time complexity:
+space complexity:
+pseudo code: 
+1. create object(numbers) to compare value
+2. loop through (nums) array
+	a. create variable (currentNum)
+	b. create variable (neededNum) from target - currentNum
+	c. create condition to see if targetNum is inside (numbers) object
+		i. if targetNum is inside, return mathing index in an array
+		ii. else, add values to our object
+3. return empty array
+*/
