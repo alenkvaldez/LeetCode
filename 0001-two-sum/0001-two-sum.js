3df2664d90c7b8af973e2a728ae0ee48707cf908
Alen Valdez <116237490+alenkvaldez@users.noreply.github.com>
2023-03-06 21:43:22 -0800

    Time: 68 ms (83.03%), Space: 41.9 MB (90.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,18 +4,19 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let numbers = {};
-    for (let i = 0; i < nums.length; i++){
-        let currentNum = nums[i];
-        let neededNum = target - currentNum;
-        if (neededNum in numbers){
-            return [numbers[neededNum], i]
-            } else {
-                numbers[currentNum] = i;
-            }
-        }
+    let numbers = {}
+    for (let i = 0; i < nums.length; i++){ 
+    let currentNum = nums[i];
+    let neededNum = target - currentNum
+    if (neededNum in numbers){
+        return [numbers[neededNum], i]
+    } else {
+        numbers[currentNum] = i;
+    }
+}
     return [];
 };
+
 
 
 /*
@@ -24,10 +25,10 @@ space complexity: O(n) : object
 pseudo code: 
 1. create object(numbers) to compare value
 2. loop through (nums) array
-	a. create variable (currentNum)
+	a. create variable (currentNum) for nums[i]
 	b. create variable (neededNum) from target - currentNum
-	c. create condition to see if targetNum is inside (numbers) object
-		i. if targetNum is inside, return mathing index in an array
+	c. create condition with if statement to see if neededNum is in (numbers) object
+		i. if targetNum is inside, return matching index in an array
 		ii. else, add values to our object
 3. return empty array
 */
