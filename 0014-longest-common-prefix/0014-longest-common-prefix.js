6fa3e5c5f8fda669367cdd56c4e919a98e0b3c36
Alen Valdez <116237490+alenkvaldez@users.noreply.github.com>
2023-03-06 21:20:04 -0800

    Time: 67 ms (67.34%), Space: 42.4 MB (63.87%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,25 +3,25 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    let prefix = "";
-    for(let i = 0; i < strs[0].length; i++){
-        for (let j = 1; j < strs.length; j++){ 
+  let prefix = "";
+    for (let i = 0; i < strs[0].length; i++ ){
+        for (let j = 0; j < strs.length; j++){
             if (strs[0][i] !== strs[j][i]){
                 return prefix;
             }
         }
         prefix += strs[0][i]
     }
-    return prefix;
+    return prefix
 };
 
 
 
 /*
 1. create prefix variable as empty string
-2. loop over strs of first index
-    a. loop over the rest over strs array at index of 0
-        i. compare the character from outer loop w inner loop j
+2. loop over strs length of first index(0)
+    a. loop over the rest over strs array at index
+        i. compare the character from outer loop i with inner loop j
             i. if !== strs, return prefix
     b. concat prefix w outer loop variable
 3. return prefix
