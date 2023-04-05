abecec9634d11ee7434e598ab91eb652ca1de1c9
Alen Valdez <116237490+alenkvaldez@users.noreply.github.com>
2023-04-05 16:45:06 -0700

    Time: 182 ms (74.71%), Space: 56.5 MB (51.13%) - LeetHub

---
+++ 0015-3sum/0015-3sum.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    const res = [];
+    nums.sort((a,b) => a-b)
+    // return result as list of lists
+    // sort input array
+    
+    for (let i = 0; i < nums.length; i++){
+        const a = nums[i];
+        if (a > 0) break;
+        if (i > 0 && a === nums[i - 1]) continue;
+    // iterate thru index and value
+        // if value is same as before, continue
+        
+        let l = i + 1;
+        let r = nums.length - 1;
+        while (l < r) {
+            const threeSum = a + nums[l] + nums[r]
+            if (threeSum > 0){
+                r--;
+            } else if (threeSum < 0){
+                l++;
+            } else {
+                res.push([a, nums[l], nums[r]]);
+                l++;
+                r--;
+                while (nums[l] === nums[l - 1] && l < r){
+                    l++;
+                }
+            }
+        }
+    }
+    return res;
+}
