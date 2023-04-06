40683583deafbac6e305a5365d85d2ad4ff25cc2
Alen Valdez <116237490+alenkvaldez@users.noreply.github.com>
2023-04-06 12:17:27 -0700

    Time: 80 ms (51.63%), Space: 50.2 MB (9.00%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,33 +3,34 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let [left, right, max] = [0, height.length -1, 0];
-    // set pointers to left(0) and right(lxh - 1)
-    
-    while (left < right){
-        // l must be <  r
-        const [leftHeight, rightHeight] = getHeights(height, left, right);
-        const area = getArea(height, left, right);
-        
-        max = Math.max(max, area);
-        
-        const isRightGreater = leftHeight <= rightHeight;
-        if (isRightGreater) left++;
-        // increment pointer to maximaize height
-        
-        const isRightLess = rightHeight < leftHeight;
-        if (isRightLess) right--;
-        // decrement pointer 
+    let leftPointer = 0;
+    let rightPointer = height.length -1;
+    let maxAreaCalculated = findArea(leftPointer, rightPointer, height)
+    while(leftPointer < rightPointer){
+        if(height[leftPointer] < height[rightPointer]){
+            leftPointer++
+        } else {
+            rightPointer--
+        }
+        const currentArea = findArea(leftPointer, rightPointer, height)
+        maxAreaCalculated = Math.max(currentArea, maxAreaCalculated)
     }
-    return max;
+    return maxAreaCalculated
 };
-
-const getHeights = (height, left, right) => [height[left], height[right]];
-
-const getArea = (height, left, right) => {
-    const [leftHeight, rightHeight] = getHeights(height, left, right);
-    const _height = Math.min(leftHeight, rightHeight);
-    const width = right - left;
     
-    return _height * width;
-};
+var findArea = function(leftPointer, rightPointer, height){
+    const width = rightPointer - leftPointer 
+    const length = Math.min(height[leftPointer], height[rightPointer])
+    const area = width * length;
+    return area
+}
+
+
+//1. create leftPointer and assign to 0
+//2. create rightPointer and assign to the height.length - 1
+//3. create maxArea var and assign to current area with starting pointers
+//4. while(loop) leftPointer is less than rightPointer
+    //a. if index value at leftPointer is less than index value at rightPointer, increment leftPointer 
+    //b. else decrement right pointer 
+    //c. calculate area and then take max of current area and maxArea
+//5. return maxArea
